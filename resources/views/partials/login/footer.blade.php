1c3f27177b9fbd2e29e600b8eb95da17d52eaa16
khanrayees <rayeesinfotech@gmail.com>
2023-11-14 17:39:31 +0530

    second

--- resources/views/partials/login/footer.blade.php
+++ resources/views/partials/login/footer.blade.php
@@ -8,7 +8,7 @@
 
     <div class="navbar-collapse collapse" id="navbar-footer">
 					<span class="navbar-text">
-						&copy; {{ date('Y') }}. <a href="#">{{ Qs::getSystemName() }}</a> by <a href="#" >CJ Inspired</a>
+						&copy; {{ date('Y') }}. <a href="#">{{ Qs::getSystemName() }}</a> by <a href="https://digisoftron.com/" target="_blank">Digi Softron Technology</a>
 					</span>
 
         <ul class="navbar-nav ml-lg-auto">
